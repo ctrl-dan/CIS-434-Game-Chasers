519042ee3c01a02d7608b50f209e0b281448c704
ctrl-dan <71346088+ctrl-dan@users.noreply.github.com>
2020-09-15 15:34:10 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+  	
+    <div className="App">
+      <header className="App-header">
+		<h1>
+          GAME CHASERS!
+        </h1>
+      </header>
+       <body className="App-body">
+      
+        
+  	
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+       </p>
+        <a className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"> Click here for help setting up React!
+        </a>
+        
+		<p>
+          We're doing great team, keep it up!
+          
+        </p>
+        <img src="home.gif" alt="" width="500" height="333"/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
